a4f245dced1446d2ff0f84854fd6b827f2a3b9a5
CWD <cwd@cloudspokes.com>
2015-06-18 11:44:39 -0400

    making security configurable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var config=require('config');
 var auth = require('http-auth');
 var express = require('express');
 var path = require('path');
@@ -8,59 +9,62 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
-var basic = auth.basic({
-    realm: "Topcoder",
-    file: __dirname + "/data/users.htpasswd" // gevorg:gpass, Sarah:testpass ...
-});
-
 // Application setup.
-var app = express();
-app.use(auth.connect(basic));
+  var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  if(config.SECURESITE==='true'){
+    var basic = auth.basic({
+      realm: "Topcoder",
+      file: __dirname + "/data/users.htpasswd" // gevorg:gpass, Sarah:testpass ...
+    });
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(auth.connect(basic));
+  }
 
-app.use('/', routes);
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(__dirname + '/public/favicon.ico'));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// error handlers
+  app.use('/', routes);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handlers
+
+  // development error handler
+  // will print stacktrace
+  if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      });
+    });
+  }
+
+  // production error handler
+  // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
-      error: err
+      error: {}
     });
   });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
 
 module.exports = app;
