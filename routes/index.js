cdb2095cc3e68ae7e09f87a8c924ce9f6358ae77
CWD <cwd@cloudspokes.com>
2015-09-08 13:42:19 -0400

    updating to loop the "/public/subs" folder for rapid prototype stand up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
+var fs = require("fs");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var subsFolder = __dirname + '/../public/subs';
+
+    fs.exists(subsFolder, function(blnExists) {
+        if (blnExists) {
+            fs.readdir(subsFolder, function(err, list) {
+                if (err) {
+                    res.status(500).json(err);
+                } else {
+                    var sublist = [];
+
+                    for (var i = 0; i < list.length; ++i) {
+                        sublist[i] = {
+                            path: '/subs/' + list[i],
+                            name: list[i]
+                        };
+                    }
+
+                    res.render('index', {
+                        title: 'The Submissions',
+                        sublist: sublist
+                    });
+                }
+            });
+        } else {
+            res.render('index', {
+                title: 'dein Prototypen!',
+                sublist: []
+            });
+        }
+    });
 });
 
 module.exports = router;
